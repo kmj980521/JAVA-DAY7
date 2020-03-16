d15995ec4d3377a33c3ecdb4eb5cd8724ebec13b
MinJae <guswlfahs123@naver.com>
2020-03-16 20:11:40 +0900

    pr7.java 패키지와 import 실습

---
+++ pr7.java
@@ -0,0 +1,20 @@
+package ex1.io;
+
+public class HowAreYou1 {
+
+		private int a;
+		private int b;
+		public HowAreYou1(int a,int b)
+		{
+			this.a=a; this.b=b;
+		}
+		public int geta() 
+		{ return a;}
+		public int getb()
+		{return b;}
+		public void print() {System.out.println("드디어 해결했다");}
+		
+}
+
+
+
