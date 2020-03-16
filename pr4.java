db52fb5ccc140922cc6730cb13daeb24cdd9d558
MinJae <guswlfahs123@naver.com>
2020-03-16 18:22:27 +0900

    pr4.java on-demand형 import선언

---
+++ pr4.java
@@ -0,0 +1,12 @@
+package ex1.io;
+import java.util.*; //on-demand형 import선언. util패키지의 모든 클래스를 import 
+
+public class HowAreYou1 {
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
+
